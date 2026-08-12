599902342691c3f74087f838ffa5a8948bca6a7f
edr-lee <204632833+edr-lee@users.noreply.github.com>
2026-08-12 14:37:28 +1200

    Fix geometry of item buttons

--- main.py
+++ main.py
@@ -1,8 +1,11 @@
 """Takeaway system GUI."""
+
 from takeaway import Item
 
 from tkinter import Tk
-from tkinter.ttk import Button, Label, Entry, Frame
+from tkinter import Button as Button
+from tkinter.ttk import Button as TtkButton
+from tkinter.ttk import Label, Entry, Frame
 import tkinter.messagebox as messagebox
 
 from takeaway.cart import Cart
@@ -10,6 +13,7 @@ from takeaway.dishes import ITEMS
 
 cart: Cart
 frames: list[Frame] = []
+
 
 def main() -> None:
     root = Tk()
@@ -25,6 +29,7 @@ def main() -> None:
 
 def switch_frames(index: int) -> None:
     frames[index].tkraise()
+
 
 def get_greet_frame(root: Tk) -> Frame:
     frame = Frame(root)
@@ -46,14 +51,14 @@ def get_greet_frame(root: Tk) -> Frame:
         # valid input: create cart and switch to ordering frame
         global cart
         cart = Cart(name)
-        switch_frames(1) # TODO: this is terrible
+        switch_frames(1)  # TODO: this is terrible
 
-
-    Button(frame, text="Start ordering", command=start_ordering).grid(
+    TtkButton(frame, text="Start ordering", command=start_ordering).grid(
         row=2, column=0, columnspan=2
     )
 
     return frame
+
 
 def get_ordering_frame(root: Tk) -> Frame:
     def add_to_cart(item: Item) -> None:
@@ -62,19 +67,25 @@ def get_ordering_frame(root: Tk) -> Frame:
     frame = Frame(root)
     frame.grid(row=0, column=0, sticky="nsew")
 
-    Label(frame, text="Takeaway Ordering System!").grid(
-        row=0, column=0, columnspan=5
-    )
+    Label(frame, text="Takeaway Ordering System!").grid(row=0, column=0, columnspan=5)
 
-    # Show all items in a 5x5 grid
+    # Show all items in a 5x? grid
     for i, item in enumerate(ITEMS):
         row = (i // 5) + 1
-        col = i % 5
-        Button(frame, text=str(item), command=lambda item=item: add_to_cart(item)).grid(
-            row=row, column=col, width=10, height=10, padx=5, pady=5
-        )
+        col = i % 5  # wrap over if we already have 5 in the current row
+        # Yes, we are using unstyled tkinter button.
+        # Ttk/themed tkinter does not allow us to change the width/height,
+        # at least on macOS.
+        Button(
+            frame,
+            width=20,
+            height=3,
+            text=str(item),
+            command=lambda item=item: add_to_cart(item),
+        ).grid(row=row, column=col, padx=5, pady=5)
 
     return frame
+
 
 if __name__ == "__main__":
     main()
