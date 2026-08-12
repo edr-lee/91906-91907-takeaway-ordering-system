f7cec142abfb109b8fc2dc9f6ce863a5afb15546
edr-lee <204632833+edr-lee@users.noreply.github.com>
2026-08-12 14:21:45 +1200

    Implement greet and ordering frame

--- main.py
+++ main.py
@@ -1,59 +1,80 @@
-"""Takeaway system TUI."""
+"""Takeaway system GUI."""
+from takeaway import Item
+
+from tkinter import Tk
+from tkinter.ttk import Button, Label, Entry, Frame
+import tkinter.messagebox as messagebox
 
 from takeaway.cart import Cart
 from takeaway.dishes import ITEMS
 
+cart: Cart
+frames: list[Frame] = []
+
+def main() -> None:
+    root = Tk()
+    root.title("Takeaway Ordering System")
+
+    greet_frame = get_greet_frame(root)
+    frames.append(greet_frame)
+    frames.append(get_ordering_frame(root))
+
+    greet_frame.tkraise()
+    root.mainloop()
+
+
+def switch_frames(index: int) -> None:
+    frames[index].tkraise()
+
+def get_greet_frame(root: Tk) -> Frame:
+    frame = Frame(root)
+    frame.grid(row=0, column=0, sticky="nsew")
+
+    Label(frame, text="Welcome to the Takeaway Ordering System!").grid(
+        row=0, column=0, columnspan=2
+    )
+    Label(frame, text="Please enter your name:").grid(row=1, column=0)
+    name_entry = Entry(frame)
+    name_entry.grid(row=1, column=1)
+
+    def start_ordering() -> None:
+        name = name_entry.get().strip()
+        if name == "":
+            messagebox.showerror("Invalid name", "Name cannot be empty.")
+            return
+
+        # valid input: create cart and switch to ordering frame
+        global cart
+        cart = Cart(name)
+        switch_frames(1) # TODO: this is terrible
+
+
+    Button(frame, text="Start ordering", command=start_ordering).grid(
+        row=2, column=0, columnspan=2
+    )
+
+    return frame
+
+def get_ordering_frame(root: Tk) -> Frame:
+    def add_to_cart(item: Item) -> None:
+        pass
+
+    frame = Frame(root)
+    frame.grid(row=0, column=0, sticky="nsew")
+
+    Label(frame, text="Takeaway Ordering System!").grid(
+        row=0, column=0, columnspan=5
+    )
+
+    # Show all items in a 5x5 grid
+    for i, item in enumerate(ITEMS):
+        row = (i // 5) + 1
+        col = i % 5
+        Button(frame, text=str(item), command=lambda item=item: add_to_cart(item)).grid(
+            row=row, column=col, width=10, height=10, padx=5, pady=5
+        )
+
+    return frame
+
 if __name__ == "__main__":
-    print("Welcome to the takeaway ordering system!")
-    name = input("Please enter your name: ")
-    cart = Cart(name)
-    print(f"Hello {name}!")
-
-    while True:
-        print()
-        print("Please select an item from the menu:")
-
-        for i, item in enumerate(ITEMS):
-            print(f"{i + 1}. {item}")
-
-        print("-----")
-        print("l. List items in cart")
-        print("c. Checkout")
-
-        print()
-        choice: str = input("Enter your choice: ")
-
-        # Cannot use match statement here, as we are using Python 3.9 :/
-        if choice == "l":
-            if len(cart.items) == 0:
-                print("Your cart is empty.")
-            else:
-                print("Items in your cart:")
-                for cart_item in cart.items:
-                    print(
-                        f"{cart_item.quantity} x {cart_item.item.name} (total ${cart_item.total_price_without_tax():.2f})"
-                    )
-        elif choice == "c":
-            break
-        else:
-            try:
-                choice_int = int(choice)
-                if not (0 <= choice_int < len(ITEMS)):
-                    print("Invalid choice. Please try again.")
-                    continue
-
-                item = ITEMS[choice_int - 1]
-                cart.add_item(item, 1)
-                print(f"Added {item.name} to cart!")
-            except ValueError:
-                print("Invalid choice. Please try again.")
-                continue
-
-    if len(cart.items) > 0:
-        print(f"{cart.name}, thank you for ordering!")
-        print("Items you have purchased:")
-        for cart_item in cart.items:
-            print(
-                f"{cart_item.quantity} x {cart_item.item.name} (total ${cart_item.total_price_without_tax():.2f})"
-            )
-        print(f"Your total is: ${cart.total_price():.2f}")
+    main()
