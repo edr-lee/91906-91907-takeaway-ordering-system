dbd7fe41636e5e01e789284ad616ec9d9ad8c14f
edr-lee <204632833+edr-lee@users.noreply.github.com>
2026-08-26 13:17:17 +1200

    Deserialize item data

--- takeaway/data.py
+++ takeaway/data.py
@@ -1,4 +1,5 @@
 from __future__ import annotations
+from takeaway import Dish, Drink, Item
 
 import os
 from json import loads
@@ -6,6 +7,7 @@ from pathlib import Path
 from typing import Any
 
 takeaway_json_path = Path(os.path.realpath(__file__)).parent.parent / "takeaway.json"
+items_json_path = Path(os.path.realpath(__file__)).parent.parent / "items.json"
 
 
 class TakeawayData:
@@ -31,4 +33,101 @@ class TakeawayData:
         with open(path_to_data_json, "w") as file:
             file.write("""{
   "name": "Takeaway Name"
+}""")
+
+class ItemsData:
+    items: list[Item]
+
+    _raw: dict[str, Any]
+
+    def __init__(self, path_to_data_json: str | os.PathLike[str] | None = None):
+        if path_to_data_json is None:
+            path_to_data_json = items_json_path
+
+        try:
+            with open(path_to_data_json, "r") as file:
+                self._raw = loads(file.read())
+            # deserialize into our item classes
+            self.items = []
+            for item in self._raw["items"]:
+                if item["type"] == "dish":
+                    self.items.append(
+                        Dish(
+                            item["name"],
+                            item["price_without_tax"],
+                            item["image_filename"],
+                        )
+                    )
+                elif item["type"] == "drink":
+                    self.items.append(
+                        Drink(
+                            item["name"],
+                            item["price_without_tax"],
+                            item["size"],
+                            item["image_filename"],
+                        )
+                    )
+                else:
+                    raise ValueError(f"Invalid item type: {item['type']}")
+        except (KeyError, FileNotFoundError):
+            self.create_template(path_to_data_json)
+            self.__init__(path_to_data_json)
+
+    def create_template(self, path_to_data_json: str | os.PathLike[str]) -> None:
+        with open(path_to_data_json, "w") as file:
+            file.write("""{
+  "items": [
+    {
+      "name": "Fish and Chips",
+      "price_without_tax": 13.99,
+      "image_filename": "fishandchips.jpg",
+      "type": "dish"
+    },
+    {
+      "name": "Chicken Katsu Curry",
+      "price_without_tax": 14.99,
+      "image_filename": "katsucurry.jpg",
+      "type": "dish"
+    },
+    {
+      "name": "Beef Burger and Fries",
+      "price_without_tax": 12.49,
+      "image_filename": "burger.jpg",
+      "type": "dish"
+    },
+    {
+      "name": "Vegetable Stir Fry",
+      "price_without_tax": 11.50,
+      "image_filename": "stirfry.jpg",
+      "type": "dish"
+    },
+    {
+      "name": "Coca Cola",
+      "price_without_tax": 2.50,
+      "size": "1.5L",
+      "image_filename": "cocacola.jpg",
+      "type": "drink"
+    },
+    {
+      "name": "Lemonade",
+      "price_without_tax": 2.50,
+      "size": "1.5L",
+      "image_filename": "lemonade.jpg",
+      "type": "drink"
+    },
+    {
+      "name": "Orange Juice",
+      "price_without_tax": 2.20,
+      "size": "500ml",
+      "image_filename": "orangejuice.jpg",
+      "type": "drink"
+    },
+    {
+      "name": "Sparkling Water",
+      "price_without_tax": 1.80,
+      "size": "500ml",
+      "image_filename": "sparklingwater.jpg",
+      "type": "drink"
+    }
+  ]
 }""")
--- takeaway/dishes.py
+++ takeaway/dishes.py
@@ -1,17 +1,6 @@
-from . import Dish, Drink, Item
+from . import Dish, Drink
+from .data import ItemsData
 
-DISHES: list[Dish] = [
-    Dish("Fish and Chips", 13.99, "fishandchips.jpg"),
-    Dish("Chicken Katsu Curry", 14.99, "katsucurry.jpg"),
-    Dish("Beef Burger and Fries", 12.49, "burger.jpg"),
-    Dish("Vegetable Stir Fry", 11.50, "stirfry.jpg"),
-]
-
-DRINKS: list[Drink] = [
-    Drink("Coca Cola", 2.50, "1.5L", "cocacola.jpg"),
-    Drink("Lemonade", 2.50, "1.5L", "lemonade.jpg"),
-    Drink("Orange Juice", 2.20, "500ml", "orangejuice.jpg"),
-    Drink("Sparkling Water", 1.80, "500ml", "sparklingwater.jpg"),
-]
-
-ITEMS: list[Item] = [*DISHES, *DRINKS]
+ITEMS = ItemsData().items
+DISHES: list[Dish] = [item for item in ITEMS if isinstance(item, Dish)]
+DRINKS: list[Drink] = [item for item in ITEMS if isinstance(item, Drink)]
